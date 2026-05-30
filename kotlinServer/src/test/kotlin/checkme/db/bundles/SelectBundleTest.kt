738a3930376e64b819e1dc21cf0599b9cdece419
Anya Aladina <anka.aladjina@yandex.ru>
2026-05-30 11:37:57 +0300

    Added migrations db script(refs #87)

--- kotlinServer/src/test/kotlin/checkme/db/bundles/SelectBundleTest.kt
+++ kotlinServer/src/test/kotlin/checkme/db/bundles/SelectBundleTest.kt
@@ -36,10 +36,8 @@ class SelectBundleTest : TestcontainerSpec({ context ->
                 ).shouldNotBeNull()
             }
         val tasksIdInDB = tasksOperations.selectAllTask()
-        val hiddenTasksIdInDB = tasksOperations.selectHiddenTasks()
         val validBundleTasks: List<TaskAndOrder> = listOf(
             TaskAndOrder(tasksIdInDB[0], 1),
-            TaskAndOrder(hiddenTasksIdInDB[0], 2),
             TaskAndOrder(tasksIdInDB[1], 3),
         )
         insertedBundleTasks =
