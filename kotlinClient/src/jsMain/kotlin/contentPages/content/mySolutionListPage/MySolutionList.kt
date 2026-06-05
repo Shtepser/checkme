c6e898434d85a754679327f5c135b80549363e77
ElMakro <87662843+ElMakro@users.noreply.github.com>
2026-06-05 13:54:04 +0300

    Upgraded my solutions page (#108)

--- kotlinClient/src/jsMain/kotlin/contentPages/content/mySolutionListPage/MySolutionList.kt
+++ kotlinClient/src/jsMain/kotlin/contentPages/content/mySolutionListPage/MySolutionList.kt
@@ -1,5 +1,6 @@
 package ru.yarsu.contentPages.content.mySolutionListPage
 
+import io.kvision.core.onClick
 import io.kvision.html.Div
 import io.kvision.html.h2
 import io.kvision.panel.SimplePanel
@@ -7,28 +8,30 @@ import io.kvision.rest.HttpMethod
 import io.kvision.routing.Routing
 import kotlinx.browser.window
 import kotlinx.serialization.json.Json
-import org.w3c.fetch.RequestInit
 import ru.yarsu.contentPages.content.createRequestHeaders
-import ru.yarsu.localStorage.UserInformationStorage
 import ru.yarsu.serializableClasses.ResponseError
-import ru.yarsu.serializableClasses.solution.SolutionInListFormat
+import ru.yarsu.serializableClasses.solution.TaskOrUserSolutionsFormat
+import kotlin.uuid.Uuid
 
 class MySolutionList(
+    taskId: Uuid?,
     serverUrl: String,
     private val routing: Routing
 ) : SimplePanel() {
     init {
-        h2("Решения")
         val requestInit = createRequestHeaders(HttpMethod.GET)
-        window.fetch(serverUrl + "solution/me", requestInit).then { response ->
+        window.fetch(serverUrl + "solution/user-and-task/${taskId.toString()}", requestInit).then { response ->
             when (response.status.toInt()) {
                 200 -> response.json().then {
                     val jsonString = JSON.stringify(it)
-                    val solutionList = Json.decodeFromString<List<SolutionInListFormat>>(jsonString)
-                    if (solutionList.isEmpty()) {
+                    val taskSolutions = Json.decodeFromString<TaskOrUserSolutionsFormat>(jsonString)
+                    h2("Ваши решения задачи: ${taskSolutions.name}", className = "h2-task-link").onClick {
+                        routing.navigate("task/${taskId}")
+                    }
+                    if (taskSolutions.solutions.isEmpty()) {
                         this.add(Div("Решения не найдены"))
                     } else {
-                        this.add(MySolutionListViewer(solutionList, routing))
+                        this.add(MySolutionListViewer(taskSolutions.solutions, routing))
                     }
                 }
 
